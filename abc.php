ddcb3877a80b9cb180e6cda11cff80b813ae07ef
akankshaa10 <128207627+akankshaa10@users.noreply.github.com>
2024-05-15 22:07:51 +0530

    Add files via upload

---
+++ abc.php
@@ -0,0 +1,117 @@
+   
+   <!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+  * {
+  box-sizing: border-box;
+}
+  
+
+.container {
+  position:center;
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.container img {vertical-align: center;}
+
+.container .content {
+  position:absolute;
+  bottom: 0;
+  background: rgb(0, 0, 0); /* Fallback color */
+  background: rgba(0, 0, 0, 0.5); /* Black background with 0.5 opacity */
+  color: #f1f1f1;
+  width: 50%;
+  padding: 10px;
+  font-size: 19.5px;
+}
+
+body {
+  font-family: 'georgia';
+}
+
+
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 10px 10px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+button {
+  background-color:  DodgerBlue;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+.cancelbtn {
+  width: auto;
+  padding: 10px 18px;
+  background-color: DodgerBlue;
+}
+
+ 
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+  span.psw {
+     display: block;
+     float: center;
+  }
+  .cancelbtn {
+     width: 100%;
+  }
+}
+
+body {
+  overflow-y: hidden; /* Hide vertical scrollbar */
+  overflow-x: hidden; /* Hide horizontal scrollbar */
+}
+</style>
+</head>
+<body>
+
+
+ 
+  <div class="container">
+  <img src="r2.JPEG" height="100%", alt="Notebook" style="width:100%;">
+  <div class="content">
+ 
+ 
+
+  
+<div class="bg-img"> 
+<body bgcolor="Black">
+
+   <form action="file:///C:/Users/Shruti/Desktop/ADD.HTML">
+
+
+<h1>E-HEALTHCARE ONLINE CONSULTATION AND MEDICAL SUBSCRIPTION</h1><BR><BR>
+<marquee>WELCOME TO E-HEALTHCARE ONLINE CONSULTATION AND MEDICAL SUBSCRIPTION </marquee>
+<img src="C:\css\wppppp.JPEG" height="100%", alt="Notebook" style="width:100%;"> 
+<P>WELCOME TO E-HEALTHBCARE ONLINE CONSULTATION AND MEDICAL SUBSCRIPTION...
+Here we propose a system that connects patients to available doctors for online consultation.
+Our proposed system aims to build an environment where various patients needing doctor help at their home.
+Doctors can handle emergency situation by providing primary help, till the patient can be taken to the hospital. 
+at emergency situation go to contact us for quick primary consultation or if have some query about the system send your feedback in feedback section..</P>
+<button type="submit"> NEXT<<
+ 
+</button>
+    <label>
+ 
+</div>
+</head>
+</body>
+</html>
---
+++ abcd9.php
@@ -0,0 +1,141 @@
+  <?php 
+
+    require_once("connection11.php");
+    $query = " select * from ash";
+    $result = mysqli_query($con,$query);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>View patients</title>
+</head>
+ <style type='text/css'>
+   
+  
+  h1 {
+    color:pink;
+}
+
+h2 {
+    color:WHITE;
+;
+}
+
+
+<head>
+    <meta charset="UTF-8">
+    <title>GFG User Details</title>
+    <!-- CSS FOR STYLING THE PAGE -->
+    <style>
+        table {
+            margin: 0 auto;
+            font-size: large;
+            border: 1px solid black;
+        }
+  
+        h1 {
+            text-align: center;
+            color: #006600;
+            font-size: xx-large;
+            font-family: 'georgia';
+        }
+  
+        td {
+            background-color: #E4F5D4;
+            border: 1px solid black;
+        }
+  
+        th,
+        td {
+            font-weight: bold;
+            border: 1px solid black;
+            padding: 10px;
+            text-align: center;
+        }
+  
+        td {
+            font-weight: lighter;
+        }
+                    .button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: green;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+}
+
+.button:hover {
+  background-color: green;
+}
+.button1{
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 20px;
+  color: white;
+  background-color:  #072F5F;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+}
+
+.button1:hover {
+  background-color: #072F5F;
+}
+    </style>
+</head>
+   
+   
+</style>
+ 
+
+  <h1><img src="https://images.vexels.com/media/users/3/128044/isolated/preview/94f0b955807b088de5dc109c019b6fc9-health-care-medical-sign-by-vexels.png" style="width:10%">E HEALTHCARE ONLINE CONSULTATION AND MEDICAL SUBSCRIPTION</h1>
+  
+ <center><h1>VIEW PATIENTS</h1><center>
+  <button class="button" onclick ="document.location='con8.php'">NEXT<< </a></button>
+  <body>
+    <section> <!-- TABLE CONSTRUCTION-->
+        <table>
+                            <tr> 
+                                <td> Username </td> 
+                                <td> Mobile no</td> 
+                                <td>Appointment Date</td> 
+                            </tr>
+              <?php 
+                                    
+                                    while($row=mysqli_fetch_assoc($result))
+                                    {
+                                        $username = $row['username'];
+                                        $mobile = $row['mobile'];
+                                       
+                                        $des = $row['des'];
+                                         
+                            ?>
+                                    <tr>
+                                        <td><?php echo $username ?></td>
+                                        <td><?php echo $mobile ?></td>
+                                        <td><?php echo $des ?></td>
+                                        
+                                        
+                                    </tr>        
+                            <?php 
+                                    }  
+                            ?>                                                                    
+                                   
+ 
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+</body>
